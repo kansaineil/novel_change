98183eee5ffd73a9e371d2b4629b2e7a950ed7f2
mariavmunteanu <maria.v.munteanu@gmail.com>
2024-08-16 20:50:18 -0400

    change frequencies with total counts

--- Collocation/datatable/add_frequencies.py
+++ Collocation/datatable/add_frequencies.py
@@ -4,9 +4,14 @@ file_name_excel="1991datatable.xlsx"
 file_name_txt="all_lemma.txt"
 year="1991"
 
-df = pd.read_excel(file_name_excel)
 
+# read all_lemma.txt and create a map where key is "lemma:year" and value is token from that line
 
+# tokens[0] is the index
+# tokens[1] is the lemma
+# tokens[2] is the total matches for that lemma
+# tokens[3] is the year for
+# tokens[4] is the total matches for all lemmas
 
 with open(file_name_txt, encoding="utf-8") as  f:
     lines=f.readlines()
@@ -24,7 +29,13 @@ for i in range(len(lines)):
     map_lemmas[key]=tokens
 
     
-frequencies=[]
+lemma_totals=[]
+
+#match data`
+# read the excel file line by line
+# for each "Relevant Lemma" find the total matches in the year and add them in a new column
+# the new column has the header count of all lemma_tokens
+df = pd.read_excel(file_name_excel)
 
 for i in range(len(df["Relevant Lemma"].values)):
     lemma=df["Relevant Lemma"].values[i]
@@ -32,16 +43,20 @@ for i in range(len(df["Relevant Lemma"].values)):
     print(f"{key=}")
     if key in map_lemmas.keys():
         print(f"{map_lemmas[key]=}")
-        frequency=int(map_lemmas[key][2]) / int(map_lemmas[key][4])
+        lemmatotal=int(map_lemmas[key][2])
         
     else:
         print(f"{key} not found")
-        frequency="N/A"
-    print(f"{frequency=}")
-    frequencies.append(frequency)
+        lemmatotal="N/A"
+    print(f"{lemmatotal=}")
+    lemma_totals.append(lemmatotal)
     print("\n")
+
+#add data in excel
     
-df.insert(3, "Frequency of overall Lemma", frequencies, True)
+df.insert(3, "count_of_all_lemma_tokens", lemma_totals, True)
+
+# save excel file in the output file
 
 df.to_excel("Out_"+file_name_excel, index=False)
 
